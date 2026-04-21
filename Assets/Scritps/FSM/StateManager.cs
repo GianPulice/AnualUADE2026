eae67ecccec146e7ada00bbee0bf80358474365f
Fabrizio <69770999+fabrizio0712@users.noreply.github.com>
2026-04-21 14:08:25 -0300

    Bloqueo de movimiento + Agacharse + Correr
    implementación de las mecanicas de bloqueo de movimiento, agacharse y correr, falta aplicarlos en FSM.
    Estructura básica genérica de FSM.

---
+++ Assets/Scritps/FSM/StateManager.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+
+public abstract class StateManager<EState> : MonoBehaviour where EState : Enum
+{
+    protected Dictionary<EState,BaseState<EState>> States = new Dictionary<EState, BaseState<EState>>();
+    protected BaseState<EState> CurrentState;
+    protected bool IsTransitioningState = false;
+
+    private void Start()
+    {
+        CurrentState.EnterState();
+    }
+    private void Update()
+    {
+        EState nextStateKey = CurrentState.GetNextState();
+        if(!IsTransitioningState && nextStateKey.Equals(CurrentState.Statekey))
+        {
+            CurrentState.UpdateState();
+        }
+        else if (!IsTransitioningState)
+        {
+            TransitionToState(nextStateKey);
+        }
+    }
+    public void TransitionToState(EState stateKey) 
+    {
+        IsTransitioningState = true;
+        CurrentState.ExitState();
+        CurrentState = States[stateKey];
+        CurrentState.EnterState();
+        IsTransitioningState = false;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        CurrentState.OnTriggerEnter(other);
+    }
+    private void OnTriggerExit(Collider other) 
+    {
+        CurrentState.OnTriggerExit(other);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        CurrentState.OnTriggerStay(other);
+    }
+}
--- Assets/Scritps/Player/PlayerController.cs
+++ Assets/Scritps/Player/PlayerController.cs
@@ -10,7 +10,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform orientation;
     [SerializeField] private Transform playerBody;
     private Vector3 moveDir = Vector3.zero;
-    private float currentVelocity = 0;
+    private bool canMove = true;
+    private bool isCrouch = false;
+    private float currentVelocity = 0f;
+    private float speedMultiplier = 1f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -27,18 +30,51 @@ public class PlayerController : MonoBehaviour
     
     private void InputUpdate() 
     {
+        // Conseguir forward en función de a donde mira la cámara
         orientation.forward = (transform.position - new Vector3(cameraTransform.position.x, transform.position.y, cameraTransform.position.z)).normalized;
+        
+        // Conseguir vector dirección de movimiento segun los inputs
         moveDir = orientation.forward * Input.GetAxis("Vertical") + orientation.right * Input.GetAxis("Horizontal");
         moveDir.Normalize();
+        
+        //Mecánica Agacharse
+        if (Input.GetButtonDown("Crouch")) 
+        {
+            if (!isCrouch) 
+            {
+                isCrouch = true;
+                speedMultiplier = movement.CrouchSpeedMultiplier;
+                characterController.height = 0.9f;
+                characterController.center = new Vector3(0, 0.45f, 0);
+            }
+            else 
+            {
+                isCrouch = false;
+                speedMultiplier = 1;
+                characterController.height = 1.8f;
+                characterController.center = new Vector3(0, 0.9f, 0);
+            }
+        }
+
+        //Mecánica Correr
+        if (Input.GetButtonDown("Sprint") && !isCrouch) speedMultiplier = 1.5f;
+        else if (Input.GetButtonUp("Sprint") && !isCrouch) speedMultiplier = 1f;
     }
     private void Movement() 
     {
-        if (moveDir != Vector3.zero)
+        if (canMove)
         {
-            playerBody.forward = Vector3.Slerp(playerBody.forward, moveDir, Time.deltaTime * movement.RotationSpeed);
-            if (currentVelocity < movement.MoveSpeed) currentVelocity += movement.Acceleration * Time.deltaTime;
-            characterController.Move(playerBody.forward * currentVelocity * Time.deltaTime);
+            if (moveDir != Vector3.zero)
+            {
+                playerBody.forward = Vector3.Slerp(playerBody.forward, moveDir, Time.deltaTime * movement.RotationSpeed);
+                if (currentVelocity < movement.MoveSpeed * speedMultiplier)
+                {
+                    currentVelocity += movement.Acceleration * Time.deltaTime;
+                }
+                else currentVelocity = movement.MoveSpeed * speedMultiplier;
+                characterController.Move(playerBody.forward * currentVelocity * Time.deltaTime);
+                Debug.Log(currentVelocity);
+            }
         }
-        
     }
 }
