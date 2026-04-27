7469a91d4add47f7a2a610854caed624db32bde6
ImSkrub <estebanaberastury@gmail.com>
2026-04-26 21:48:22 -0300

    Try Fix Inventory Issue

--- Assets/Scritps/UI/Inventory/ItemSlotView.cs
+++ Assets/Scritps/UI/Inventory/ItemSlotView.cs
@@ -213,7 +213,7 @@ public class ItemSlotView : MonoBehaviour,IPointerEnterHandler, IPointerExitHand
         selectionFillImage.color = c;
     }
 
-    private void OnButtonClicked()
+    public void OnButtonClicked()
     {
         onClicked?.Invoke(Item);
     }
