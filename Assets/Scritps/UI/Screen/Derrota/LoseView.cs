b145e08d6f35206d903b1a429fd812c0f5c00eb3
ImSkrub <estebanaberastury@gmail.com>
2026-05-12 12:40:10 -0300

    Asignar view y controller
    Asunto con la nomenclatura de los botones todavia no resuelto. No se hizo las mejoras visuales del Win Canvas

--- Assets/Scritps/UI/Screen/Derrota/LoseView.cs
+++ Assets/Scritps/UI/Screen/Derrota/LoseView.cs
@@ -1,17 +1,33 @@
 using UnityEngine;
 using TMPro;
 using Cysharp.Threading.Tasks;
+using UnityEngine.UI;
+using System;
 
 public class LoseView : BaseResultView
 {
     [Header("Lose-specific")]
     [SerializeField] private TextMeshProUGUI _titleText;
+    [SerializeField] private Button _btnOptions;
 
+    public event Action OnOptionsClicked;
     protected override void Awake()
     {
         base.Awake();
         HideNextLevelButton(); // Lose screen has no "next level"
         if (_titleText != null) _titleText.text = "You lose!";
+        if (_btnOptions != null) _btnOptions.onClick.AddListener(() => OnOptionsClicked?.Invoke());
+    }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+        if(_btnOptions != null) _btnOptions.onClick.RemoveAllListeners();
+    }
+
+    private void HandleOptionsButtonClicked()
+    {
+        OnOptionsClicked?.Invoke();
     }
 
     // Override for any lose-specific animations or effects before showing the screen.
